d43800cb0fb516a91347ef47528226281227be0f
otmanTR <r.brk.otman@gmail.com>
2023-01-24 21:47:04 +0300

    added modules

---
+++ modules/dateTime.js
@@ -0,0 +1,13 @@
+import { DateTime } from './luxon.js';
+
+const today = document.getElementById('date');
+const setTime = () => {
+  const now = DateTime.now();
+  const date = now.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+  today.textContent = date;
+};
+
+setInterval(setTime, 1000);
+export {
+  setTime,
+};
